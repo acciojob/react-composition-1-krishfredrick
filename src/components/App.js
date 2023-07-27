292de00ea6b20058bc595ed0dbd0e836b7137875
Rajesh Kannan <krishfredrick11@gmail.com>
2023-07-27 19:11:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React,{useId, useState} from "react";
 import './../styles/App.css';
 
+const Tab = ({number, setState})=>{
+  
+     return (<li onClick={()=> setState(number)}>Tab {number}</li>)
+}
+
 const App = () => {
+  const [tab,setTab] = useState(1);
+
   return (
     <div>
         {/* Do not remove the main div */}
+    <ul>
+    {[1, 2, 3].map((val)=>(<Tab number={val} key={val} setState ={setState} />)}
+    </ul>
+      This is the content for {`Tab ${tab}`}.
     </div>
   )
 }
